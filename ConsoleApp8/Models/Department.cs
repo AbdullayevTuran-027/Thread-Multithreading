2f35cbb6ae0cf2deeb89bf616cf80d9bd904259f
AbdullayevTuran-027 <99797460+AbdullayevTuran-027@users.noreply.github.com>
2022-04-05 11:31:45 +0400

    wdwa
    wdw

---
+++ ConsoleApp8/Models/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8.Models
+{
+    class Department
+    {
+        private int _id;
+        public int Id { get; }
+        public List<Employe> _employees;
+
+        public Department()
+        {
+            _id++;
+            Id = _id;
+            _employees = new List<Employe>();
+        }
+        public void AddEmployee(Employe employe)
+        {
+            _employees.Add(employe);
+        }
+    }
+}
---
+++ ConsoleApp8/Models/Employe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8.Models
+{
+    class Employe
+    {
+        private int _id;
+        public int Id { get; }
+        private string Name { get; set; }
+        private double Salary { get; set; }
+
+        public Employe()
+        {
+            _id++;
+            Id = _id;
+            
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"id{Id}, Name{Name}, Salary{Salary}");
+        }
+    }
+   
+
+    
+}
